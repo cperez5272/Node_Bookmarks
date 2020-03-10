c625d266341f1bf1ee1cc96e512a49798d02567b
Cesar Perez <Cesar.Perez5272@yahoo.com>
2020-03-09 22:37:37 -0400

    'still confused :c'

--- seeds/seed.bookmark_server.sql
+++ seeds/seed.bookmark_server.sql
@@ -1,8 +1,8 @@
-INSERT INTO bookmark_server (title, url, description)
+INSERT INTO bookmarks_server (title, url, description)
 VALUES
 ('first bookmark post','mario_page.org','This is a red bookmark that resembles mario'),
 ('second bookmark post','peach_page.org','This is a pink bookmark that resembles peach'),
 ('third bookmark post','toad_page.org','This is a blue bookmark that resembles toad'),
-('fourth bookmark post','koopa_page.org','This is a orange bookmark that resembles koopa'),
+('fourth bookmark post','koopa_page.org','This is a orange bookmark that resembles koopa')
 
 -- psql -U dunder_mifflin -d bookmarks -f ./seeds/seed.bookmark_server.sql
